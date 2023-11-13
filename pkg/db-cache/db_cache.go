2f2a9adfe00c1e05a99299cb76481c277c71b6ce
Fernando Azouth <fazouth@transactrx.com>
2023-11-13 16:59:36 -0500

    Add support to use maps as the cached structures
    You can now create a cache of maps with string keys, and any interface
    for the content. This allows the cache to be used in cases where the
    corresponding structs are not predefined in code.
    
    The same existing cache rules apply. The only difference is that the
    cache will contain a list of map of entries, one per database row. Each
    entry in the cache will be keyed with a string value extracted from the
    key field specified. Since there is no underlying struct to reference,
    the key field is expected to be one of the database query fields.
    
    Additionally, each map entry will itself contain a sub-map as its
    value. The sub-map will reflect the table fields as keys, and the
    corresponding values as the map entry values.
    
    e.g
    Cache entries for a map[string]any, with query "select field1, field2,
    field3 from table", and key field being field2 would look as follows:
    
    cache *dbcache.DbCache[map[string]any] ->
    [
    value2a -> map[field1: value1a, field2: value2a, field3: value3a]
    value2b -> map[field1: value1b, field2: value2b, field3: value3b]
    value2c -> map[field1: value1c, field2: value2c, field3: value3c]
    ]

--- pkg/db-cache/db_cache.go
+++ pkg/db-cache/db_cache.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strings"
 	"sync"
 	"time"
 
@@ -189,11 +190,22 @@ func createTableMonitoringTrigger(tableName string, DB *pgxpool.Pool) error {
 }
 
 func getKeyValue(obj any, keyField string) (string, error) {
-	r := reflect.ValueOf(obj)
-	fv := reflect.Indirect(r).FieldByName(keyField)
-	if !fv.IsValid() {
-		return "", fmt.Errorf("field %s is not found in the APIKey struct", keyField)
-	}
-	return fv.Elem().String(), nil
 
+	objValue := reflect.ValueOf(obj)
+	objKind := objValue.Kind()
+	objType := objValue.Type()
+	if (objKind == reflect.Map) && (objType.Key().Kind() == reflect.String) {
+		for _, mapKey := range objValue.MapKeys() {
+			if strings.EqualFold(mapKey.String(), keyField) {
+				return objValue.MapIndex(mapKey).Interface().(string), nil
+			}
+		}
+		return "", fmt.Errorf("specified key field '%s' is not part of the query results", keyField)
+	} else {
+		fv := reflect.Indirect(objValue).FieldByName(keyField)
+		if !fv.IsValid() {
+			return "", fmt.Errorf("field %s is not found in the APIKey struct", keyField)
+		}
+		return fv.Elem().String(), nil
+	}
 }
